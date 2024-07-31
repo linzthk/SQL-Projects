b1fe8ac3868be0d839cfc200f1f1a45056bf5999
linzthk <37417488+linzthk@users.noreply.github.com>
2024-07-30 22:07:59 -0500

    Add files via upload

---
+++ modelcarstore.sql
@@ -0,0 +1,134 @@
+/* Customers: customer data
+Employees: all employee information
+Offices: sales office information
+Orders: customers' sales orders
+OrderDetails: sales order line for each sales order
+Payments: customers' payment records
+Products: a list of scale model cars
+ProductLines: a list of product line categories */
+
+
+
+SELECT 'Customers' AS table_name, 
+       13 AS number_of_attribute,
+       COUNT(*) AS number_of_row
+  FROM Customers
+  
+UNION ALL
+
+SELECT 'Products' AS table_name, 
+       9 AS number_of_attribute,
+       COUNT(*) AS number_of_row
+  FROM Products
+
+UNION ALL
+
+SELECT 'ProductLines' AS table_name, 
+       4 AS number_of_attribute,
+       COUNT(*) AS number_of_row
+  FROM ProductLines
+
+UNION ALL
+
+SELECT 'Orders' AS table_name, 
+       7 AS number_of_attribute,
+       COUNT(*) AS number_of_row
+  FROM Orders
+
+UNION ALL
+
+SELECT 'OrderDetails' AS table_name, 
+       5 AS number_of_attribute,
+       COUNT(*) AS number_of_row
+  FROM OrderDetails
+
+UNION ALL
+
+SELECT 'Payments' AS table_name, 
+       4 AS number_of_attribute,
+       COUNT(*) AS number_of_row
+  FROM Payments
+
+UNION ALL
+
+SELECT 'Employees' AS table_name, 
+       8 AS number_of_attribute,
+       COUNT(*) AS number_of_row
+  FROM Employees
+
+UNION ALL
+
+SELECT 'Offices' AS table_name, 
+       9 AS number_of_attribute,
+       COUNT(*) AS number_of_row
+  FROM Offices;
+  
+  -- Priority Products for restocking
+WITH 
+
+low_stock_table AS (
+SELECT productCode, 
+       ROUND(SUM(quantityOrdered) * 1.0/(SELECT quantityInStock
+                                           FROM products p
+                                          WHERE od.productCode = p.productCode), 2) AS low_stock
+  FROM orderdetails od
+ GROUP BY productCode
+ ORDER BY low_stock DESC
+ LIMIT 10
+)
+
+SELECT productCode, 
+       SUM(quantityOrdered * priceEach) AS prod_perf
+  FROM orderdetails od
+ WHERE productCode IN (SELECT productCode
+                         FROM low_stock_table)
+ GROUP BY productCode 
+ ORDER BY prod_perf DESC
+ LIMIT 10;
+ 
+ -- revenue by customer
+SELECT o.customerNumber, SUM(quantityOrdered * (priceEach - buyPrice)) AS profit
+  FROM products p
+  JOIN orderdetails od
+    ON p.productCode = od.productCode
+  JOIN orders o
+    ON o.orderNumber = od.orderNumber
+ GROUP BY o.customerNumber;
+ 
+
+ -- Top 5 VIP customers
+WITH 
+
+money_in_by_customer_table AS (
+SELECT o.customerNumber, SUM(quantityOrdered * (priceEach - buyPrice)) AS revenue
+  FROM products p
+  JOIN orderdetails od
+    ON p.productCode = od.productCode
+  JOIN orders o
+    ON o.orderNumber = od.orderNumber
+ GROUP BY o.customerNumber
+)
+
+SELECT contactLastName, contactFirstName, city, country, mc.revenue
+  FROM customers c
+  JOIN money_in_by_customer_table mc
+    ON mc.customerNumber = c.customerNumber
+ ORDER BY mc.revenue DESC
+ LIMIT 5;
+ 
+ 
+ -- Average of customer profits
+WITH 
+
+money_in_by_customer_table AS (
+SELECT o.customerNumber, SUM(quantityOrdered * (priceEach - buyPrice)) AS revenue
+  FROM products p
+  JOIN orderdetails od
+    ON p.productCode = od.productCode
+  JOIN orders o
+    ON o.orderNumber = od.orderNumber
+ GROUP BY o.customerNumber
+)
+
+SELECT AVG(mc.revenue) AS average_of_customer_profits
+  FROM money_in_by_customer_table mc;
